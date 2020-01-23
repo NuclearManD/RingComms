c145f1d2167bd8a640bf65d587409f8acf82c1e0
NuclearManD <dylanbrophy@gmail.com>
2020-01-23 08:51:59 -0800

    Initial Python package setup

---
+++ python/README.md
@@ -0,0 +1,7 @@
+# PyRingComms
+
+Python implementation of Nuclaer's RingComm protocol
+
+## Objectives
+
+Provide a flexible, stable, easy-to-use framework to publish and subscribe to messages over the Internet
---
+++ python/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyringcomm-Nuclaer", # Replace with your own username
+    version="0.0.1",
+    author="Nuclaer (Nuclear_Man_D)",
+    author_email="dylanbrophy@gmail.com",
+    description="Communication ring for automated systems",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/NuclearManD/RingComms",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+)
