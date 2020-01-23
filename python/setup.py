37377062987473be07dd7d57abd0e05b4d15ca79
NuclearManD <dylanbrophy@gmail.com>
2020-01-23 09:05:12 -0800

    Change package name

--- python/setup.py
+++ python/setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pyringcomm-Nuclaer", # Replace with your own username
+    name="pyringcomm",
     version="0.0.1",
     author="Nuclaer (Nuclear_Man_D)",
     author_email="dylanbrophy@gmail.com",
