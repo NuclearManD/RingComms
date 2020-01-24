882b3494b4ba9445fc331fc4662963ae6f0891c0
NuclearManD <dylanbrophy@gmail.com>
2020-01-24 09:32:41 -0800

    Communication (to a degree,.. wip)

--- python/pyringcomm/ringlink.py
+++ python/pyringcomm/ringlink.py
@@ -1,9 +1,28 @@
-import socket, _thread, json
+import socket, _thread, json, time
 
 DEFAULT_PORT = 325
 
 class ProtocolException(Exception):
     pass
+
+class test_socket:
+    def __init__(self):
+        self.buffer = b'ring'
+    def recv(self,n):
+        while self.buffer == b'':
+            time.sleep(.01)
+        dt = self.buffer[:n]
+        self.buffer = self.buffer[n:]
+        return dt
+    def wr(self, jsonobj):
+        go = json.dumps(jsonobj).encode()
+        self.buffer += len(go).to_bytes(3, 'big')
+        self.buffer += go
+    def sendall(self, d):
+        print("sendall(" + repr(d) + ')')
+
+def onrec(d, m):
+    print("Com "+repr(d)+" revc'd " + repr(m))
 
 class Communicator:
     def __init__(self, socket, on_recv_callback, on_close_callback):
@@ -19,9 +38,23 @@ class Communicator:
         self.socket.sendall(len(text).to_bytes(3, 'big'))
         self.socket.sendall(text)
     def __receiver__(self):
+        try:
+            while True:
+                lenbytes = self.socket.recv(3)
+                if len(lenbytes) != 3:
+                    break
+                length = int.from_bytes(lenbytes, 'big')
+                data = self.socket.recv(length).decode()
+                self.recv_callback(self, json.loads(data))
+        except:
+            pass
         self.close_callback(self)
+        
+
 class Client:
     def __init__(self, address, port = DEFAULT_PORT):
         self.socket = socket.socket()
         self.socket.connect((address, port))
-        
+        self.com = Communicator(sok, self.on_recv, self.on_close)
+
+
